5b7a39df0a03f8f4876494fca411843b0b490c01
wangxk <wxkskynet@vip.qq.com>
2017-07-17 00:22:29 +0800

    new chanllenges added
    Introduction Challenges

---
+++ Introduction/Java If-Else.java
@@ -0,0 +1,34 @@
+/*
+@Task 
+Given an integer, , perform the following conditional actions:
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+Complete the stub code provided in your editor to print whether or not n is weird.
+
+@Sample Input 1
+24
+
+@Sample Output 1
+Not Weird
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();            
+        String ans="";
+        if(n%2 == 1) {
+            ans = "Weird";
+        } else {
+            if(n > 5 && n <= 20) {
+                ans = "Weird";
+            } else {
+                ans = "Not Weird";                 
+            }
+        }
+        System.out.println(ans);
+    }
+}
