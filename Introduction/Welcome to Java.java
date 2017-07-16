b2bff612825d2738a19d329c552a6bee0727c3f9
wangxk <wxkgh@users.noreply.github.com>
2017-07-17 00:23:22 +0800

    Update Welcome to Java.java

--- Introduction/Welcome to Java.java
+++ Introduction/Welcome to Java.java
@@ -13,7 +13,6 @@ Print "Hello, Java." on the second line.
 */
 
 public class Solution {
-
     public static void main(String[] args) {
         /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
         System.out.println("Hello, World.");
