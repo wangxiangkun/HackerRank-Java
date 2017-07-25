ef50b18cb562cc1eda1392f10c7b2a8c5f8dad45
wangxk <wxkskynet@vip.qq.com>
2017-07-25 22:41:02 +0800

    String Regex

---
+++ Strings/Java Regex.java
@@ -0,0 +1,17 @@
+/*
+In this problem you will be provided strings containing any combination of ASCII characters.
+You have to write a regular expression to find the valid IPs.
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+    }
+}
+
+class MyRegex {
+    String pattern = "((?:(?:25[0-5]|2[0-4]\\d|(?:1\\d{2}|[1-9]?\\d))\\.){3}(?:25[0-5]|2[0-4]\\d|(?:1\\d{2}|[1-9]?\\d)))";
+}
